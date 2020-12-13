c20e034002fcf6d0b54c4772be14d11c95da289c
koskywalker <k.muraka.jp@gmail.com>
2020-12-14 01:19:28 +0900

    gatsby-source-filesystem のオプションを微修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "images",
-        path: "./src/images/",
+        path: `${__dirname}/src/images`,
       },
       __key: "images",
     },
