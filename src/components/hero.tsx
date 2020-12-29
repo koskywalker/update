45ef9eaf140871a061570cd0eaf983b14fd3ef2b
koskywalker <k.muraka.jp@gmail.com>
2020-12-29 16:29:57 +0900

    hero コンポーネントにタイピングエフェクトを追加

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,25 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import Typed from "typed.js"
+
+import { heroSentenceList } from "../constants/constants"
 
 export const Hero: React.FC = () => {
+  const [sentenceList] = useState(heroSentenceList)
+  useEffect(() => {
+    const options = {
+      strings: sentenceList,
+      typeSpeed: 100,
+      startDelay: 1000,
+      backSpeed: 20,
+      backDelay: 1500,
+      loop: true,
+    }
+    const typed = new Typed("#typing", options)
+    return () => {
+      typed.destroy()
+    }
+  }, [sentenceList])
+
   return (
     <div className="bg-gradient-to-b from-white to-gray-50">
       <div className="px-4 py-24 mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -8,9 +27,9 @@ export const Hero: React.FC = () => {
           <p className="mt-5 text-xl text-gray-500 sm:text-center">
             Hello, I am Kosuke.
           </p>
-          <h1 className="text-5xl font-extrabold text-gray-900 sm:text-center">
-            I&#39;m A Developer.
-          </h1>
+          <p className="text-5xl font-extrabold text-gray-900 sm:text-center">
+            I&#39;m <span id="typing"></span>
+          </p>
         </div>
       </div>
     </div>
---
+++ src/constants/constants.ts
@@ -0,0 +1,6 @@
+export const heroSentenceList = [
+  '<span class="text-blue-500">A Developer</span>.',
+  '<span class="text-pink-500">A Designer</span>.',
+  '<span class="text-green-500">A Blogger</span>.',
+  '<span class="text-yellow-500">An SEO Expert</span>.',
+]
