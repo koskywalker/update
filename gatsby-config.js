dfca1905bf12ed320530cc67e73e9946374b3002
koskywalker <k.muraka.jp@gmail.com>
2021-01-23 03:21:27 +0900

    feed の設定追加

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,7 @@
 require("dotenv").config({ path: ".env" })
 
+const siteTitle = "UPDATE"
+
 module.exports = {
   siteMetadata: {
     siteUrl: process.env.GATSBY_BASE_URL,
@@ -71,8 +73,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "UPDATE",
-        short_name: "UPDATE",
+        name: siteTitle,
+        short_name: siteTitle,
         start_url: "/",
         background_color: "#ffffff",
         theme_color: "#06b6d4",
@@ -92,5 +94,59 @@ module.exports = {
       __key: "images",
     },
     "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { allContentfulBlogPost } }) => {
+              return allContentfulBlogPost.edges.map(({ node }) => {
+                return {
+                  title: node.title,
+                  description: node.description.description,
+                  date: node.publishDate,
+                  url: `${process.env.GATSBY_BASE_URL}/${node.slug}`,
+                  guid: `${process.env.GATSBY_BASE_URL}/${node.slug}`,
+                }
+              })
+            },
+            query: `
+              {
+                allContentfulBlogPost(
+                  limit: 5
+                  sort: { fields: publishDate, order: DESC }
+                ) {
+                  edges {
+                    node {
+                      title
+                      slug
+                      description {
+                        description
+                      }
+                      publishDate
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: siteTitle,
+            feed_url: `${process.env.GATSBY_BASE_URL}/rss.xml`,
+            site_url: process.env.GATSBY_BASE_URL,
+          },
+        ],
+      },
+    },
   ],
 }
